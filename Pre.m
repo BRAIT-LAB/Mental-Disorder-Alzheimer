0466bc6979af5ef64541fa0c403647eda9fe4bea
liyuguanchloe <112382474+liyuguanchloe@users.noreply.github.com>
2022-09-06 14:56:25 +0800

    Update Pre.m

--- Pre.m
+++ Pre.m
@@ -1,17 +1,18 @@
 clear ;clc;
-path = ['F:\sleepdata\set'];           %改成你的路径，路径下放有需要分析的所有.set文件
+path = ['F:\sleepdata\set'];           %路径下放有需要分析的所有.set文件
 cd(path);
 AllFile = dir('**/*.*set');
 for i=1:length(AllFile)
 	file{i,1}=AllFile(i).name;
 end          
+
 %%
 mkdir(path,'\Pre');
 outpath=[path '\Pre'];
 for n = 1:length(AllFile)  
 EEG = pop_loadset('filename',file{n,1},'filepath',path);
 EEG = eeg_checkset( EEG );
-EEG = pop_chanedit(EEG, 'lookup','D:\\Matlab\\toolbox\\eeglab_current\\plugins\\dipfit\\standard_BESA\\standard-10-5-cap385.elp');%模板路径改成自己电脑EEGlab里的
+EEG = pop_chanedit(EEG, 'lookup','D:\\Matlab\\toolbox\\eeglab_current\\plugins\\dipfit\\standard_BESA\\standard-10-5-cap385.elp');
 EEG = eeg_checkset( EEG );
 EEG = pop_reref( EEG, []);
 EEG = eeg_checkset( EEG );
